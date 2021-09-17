a060c813dbefe2331b134d68e6d180829a61123a
Kyrylo Dorozhynskyi <kyrylo.dorozhynskyi@justdev.org>
2021-09-17 12:32:55 +0200

    [DEV] Function for create env file

--- scripts/Roots/Installer.php
+++ scripts/Roots/Installer.php
@@ -8,6 +8,78 @@ class Installer
 {
 	public static $KEYS = ['AUTH_KEY', 'SECURE_AUTH_KEY', 'LOGGED_IN_KEY', 'NONCE_KEY', 'AUTH_SALT', 'SECURE_AUTH_SALT', 'LOGGED_IN_SALT', 'NONCE_SALT'];
 
+	public static function addEnv(Event $event)
+	{
+		$root = dirname(dirname(__DIR__));
+		$composer = $event->getComposer();
+		$io = $event->getIO();
+		$generate_salts = $io->askConfirmation('<info>Generate .env file?</info> [<comment>Y,n</comment>]? ', true);
+
+		if (!$generate_salts) {
+			return 1;
+		}
+
+		$APP_NAME_ask = $io->ask('<info>Write youre [<comment>APP_NAME</comment>]</info> ');
+		if ($APP_NAME_ask) {
+			$APP_NAME = "APP_NAME='{$APP_NAME_ask}'\n";
+		}
+
+		$PHPMYADMIN_PORT_ask = $io->ask('<info>Write youre [<comment>PHPMYADMIN_PORT</comment>]</info> ');
+		if ($PHPMYADMIN_PORT_ask) {
+			$PHPMYADMIN_PORT = "PHPMYADMIN_PORT={$PHPMYADMIN_PORT_ask}\n";
+		}
+
+		$BACKEND_PORT_ask = $io->ask('<info>Write youre [<comment>BACKEND_PORT</comment>]</info> ');
+		if ($BACKEND_PORT_ask) {
+			$BACKEND_PORT = "BACKEND_PORT={$BACKEND_PORT_ask}\n";
+		}
+
+		$DB_NAME = "DB_NAME='{$APP_NAME_ask}'\n";
+
+		$DB_USER = "DB_USER=root\n";
+		$DB_PASSWORD = "DB_PASSWORD=dev\n";
+		$DB_HOST = "DB_HOST=${APP_NAME_ask}.mariadb:3306\n";
+
+		$DB_PORT_ask = $io->ask('<info>Write youre [<comment>DB_PORT</comment>]</info> ');
+		if ($DB_PORT_ask) {
+			$DB_PORT = "DB_PORT={$DB_PORT_ask}\n";
+		}
+
+		$WP_ENV_ask = $io->ask('<info>Write youre [<comment>WP_ENV: development/staging/production</comment>]</info> ');
+		if ($WP_ENV_ask) {
+			$WP_ENV = "WP_ENV='{$WP_ENV_ask}'\n";
+		}
+
+		$ENV_DEVELOPMENT = "ENV_DEVELOPMENT='http://localhost:${BACKEND_PORT_ask}'\n";
+
+		$ENV_STAGING_ask = $io->ask('<info>Write youre [<comment>ENV_STAGING</comment>]</info> ');
+		if ($ENV_STAGING_ask) {
+			$ENV_STAGING = "ENV_STAGING='{$ENV_STAGING_ask}'\n";
+		}
+
+		$ENV_PRODUCTION_ask = $io->ask('<info>Write youre [<comment>ENV_PRODUCTION</comment>]</info> ');
+		if ($ENV_PRODUCTION_ask) {
+			$ENV_PRODUCTION = "ENV_PRODUCTION='{$ENV_PRODUCTION_ask}'\n";
+		}
+
+		$env_content =
+			$APP_NAME .
+			$PHPMYADMIN_PORT .
+			$BACKEND_PORT .
+			$DB_NAME .
+			$DB_USER .
+			$DB_PASSWORD .
+			$DB_HOST .
+			$DB_PORT .
+			$WP_ENV .
+			$ENV_DEVELOPMENT .
+			$ENV_STAGING .
+			$ENV_PRODUCTION;
+
+		$env_file = "{$root}/.env";
+		file_put_contents($env_file, $env_content);
+	}
+
 	public static function addSalts(Event $event)
 	{
 		$root = dirname(dirname(__DIR__));
@@ -30,11 +102,15 @@ class Installer
 
 		$env_file = "{$root}/.env";
 
-		if (copy("{$root}/.env.example", $env_file)) {
+		if (file_exists($env_file)) {
 			file_put_contents($env_file, implode($salts, "\n"), FILE_APPEND | LOCK_EX);
 		} else {
-			$io->write('<error>An error occured while copying your .env file</error>');
-			return 1;
+			if (copy("{$root}/.env.example", $env_file)) {
+				file_put_contents($env_file, implode($salts, "\n"), FILE_APPEND | LOCK_EX);
+			} else {
+				$io->write('<error>An error occured while copying your .env file</error>');
+				return 1;
+			}
 		}
 	}
 
